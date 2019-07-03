1e252660ccb1dd16a94fc34fdb4206f3a4b4b1fe
Daimen <daimenambers@yahoo.com>
2019-07-03 10:27:07 -0400

    Something was wrong with the MenuScene

--- MineSweeper/MenuScene.swift
+++ MineSweeper/MenuScene.swift
@@ -9,59 +9,59 @@
 import SpriteKit
 
 class MenuScene: SKScene {
-
+    
     // MARK: Variables
     var playButton = SKSpriteNode()
     let playButtonText  = SKTexture(imageNamed: "play")
-
+    
     override func didMove(to view: SKView) {
-      playButton = SKSpriteNode(texture: playButtonText)
-      playButton.position = CGPoint(x: frame.midX, y: frame.midY)
-      self.addChild(playButton)
+        playButton = SKSpriteNode(texture: playButtonText)
+        playButton.position = CGPoint(x: frame.midX, y: frame.midY)
+        self.addChild(playButton)
     }
-
-
+    
+    
     func touchDown(atPoint pos : CGPoint) {
-
+        
     }
-
+    
     func touchMoved(toPoint pos : CGPoint) {
-
+        
     }
-
+    
     func touchUp(atPoint pos : CGPoint) {
-
+        
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
-          let pos = touch.location(in: self)
-          let node = self.atPoint(pos)
-
-          if node == playButton {
-            if let view = view {
-              let transition: SKTransition = SKTransition.fade(withDuration: 1)
-              let scene:SKScene = GameScene(size: self.size)
-              self.view?.presentScene(scene, transition: transition)
+            let pos = touch.location(in: self)
+            let node = self.atPoint(pos)
+            
+            if node == playButton {
+                if let view = view {
+                    let transition: SKTransition = SKTransition.fade(withDuration: 1)
+                    let scene:SKScene = GameScene(size: self.size)
+                    self.view?.presentScene(scene, transition: transition)
+                }
             }
-          }
         }
         //for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
-
+    
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
-
+    
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
-
-
+    
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
