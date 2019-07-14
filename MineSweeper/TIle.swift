59d3cbebce2601057a3ee9dc31f26548b139fb5f
Daimen <daimenambers@yahoo.com>
2019-07-14 16:15:46 -0400

    New pods, new Tile and Board swift files

--- MineSweeper/TIle.swift
+++ MineSweeper/TIle.swift
@@ -9,30 +9,41 @@
 import Foundation
 import SpriteKit
 
-class Tile {
-    enum State {
-        case Empty
-        case Discovered
-        case Flagged
-        case BadFlag
-        case ExplodedMine
-        case Mine
-        case FlaggedMine
-    }
-    //MARK: Variables
-    let size = 40
-    var x: Int
-    var y: Int
-    var state = State.Empty
-    var minesAround = 0
+class Tile: Equatable {
+    unowned let board: Board
+    var x, y: Int
+    var isMine: Bool
+    var isRevealed: Bool
+    var isMarked: Bool
+    var nbMineAround: Int
     
-    init(x: Int, y: Int) {
+    var sprite: SKNode!
+    
+    init(board: Board, x: Int, y: Int) {
+        self.board = board
         self.x = x
         self.y = y
+        self.isMine = false
+        self.isRevealed = false
+        self.isMarked = false
+        self.nbMineAround = 0
     }
     
-    func renderEmpty(ctx: CGContext?) {
-        let sizef = CGFloat(size)
-        ctx?.saveGState()
+    func setMine() {
+        if (!isMine) {
+            isMine = true
+            let adjacents = board.getNeighbors(self)
+            for adj in adjacents {
+                adj.nbMineAround += 1
+            }
+        }
     }
+    
+    func squareDistanceFromTile(_ tile: Tile) -> Int {
+        return (x - tile.x)*(x - tile.x) + (y - tile.y)*(y - tile.y)
+    }
+}
+
+func == (lhs: Tile, rhs: Tile) -> Bool {
+    return lhs.x == rhs.x && lhs.y == rhs.y
 }
